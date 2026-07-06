4965d4c79a43f8c5ba1c957967057b402af6010c
Liliana <liliana.castanon@gmail.com>
2026-07-06 13:04:27 -0600

    Create Convertir_Midi.py

---
+++ Convertir_Midi.py
@@ -0,0 +1,22 @@
+#Instala la librería de pretty_midi
+#pip install pretty_midi
+
+import math
+import pretty_midi
+midi_data = pretty_midi.PrettyMIDI('/content/1940.mid')
+
+freqs = []
+
+prev_end = 0
+for note in midi_data.instruments[0].notes:
+    delay = note.start - prev_end
+    if delay != 0:
+        #print(delay)
+        freqs.append((0, float(round(delay, 2))),)
+    #print(note, pretty_midi.note_number_to_hz(note.pitch))
+    freq = pretty_midi.note_number_to_hz(note.pitch)
+    duration = note.end-note.start
+    freqs.append((round(freq, 2), float(round(duration, 2))),)
+    prev_end = note.end
+
+print(freqs) # this is our list of freq/duration
